a5a93c80dababee4477e028a00de8e3635da0634
Divyansh Jain <divyansh200323@gmail.com>
2024-02-26 23:20:20 +0530

    S1L5 done

---
+++ 5 VALUES AND VARIABLES.js
@@ -0,0 +1,37 @@
+/*
+Values and Variables
+
+
+Values : Smallest Unit of Information
+
+Variables : Used to store values in computers memory
+
+ex : let firstName = "Divyansh"
+    here firstName is variable storing value 'Divyansh'
+
+A Variable is a Container used for storing data reffered as Information and the container is reffered as Variable which hold the data.
+
+Convention and rules for Declaring Variables:
+
+    CamelCase : in this convention first word is written in lowercase and others having first word as captial ex : for one Number = oneNumber 
+
+    as white spaces aren't allowed inside a variable name 
+
+        A Variable name Must follow these points :
+
+            It must Start form $, (a-z) or (A-Z);
+            White spaces aren't allowed between name;
+            Special Characters allowed are : $ , _ only ;
+            name never start from a number;
+
+
+            let myName = 'Name'; //  ✔️✔️✔️
+            let $name = 'Name' // ✔️✔️✔️
+            let 1Name = 'Name' // ❌❌❌
+            let name-1 = 'Name' // ❌❌❌
+            let name first = 'Name' // ❌❌❌
+            let Name = 'Name' // can bet used but not a good practice as camelcase isn't followed
+            let a = 'Name' // not a good approch as the variable name doen't define which type of value is stored
+            
+
+*/
