9d1f8de2cd3a73f6b9d9d65e31d2d1658492e57d
Divyansh Jain <divyansh200323@gmail.com>
2024-02-26 23:47:16 +0530

    S1L7 Done

---
+++ LET, CONST AND VAR.js
@@ -0,0 +1,17 @@
+// var : used for declaring a variable
+
+var age  = 'age';
+
+// let : similar to var but having a better scope compared to var
+// introduced in ES6 also reffered as mordern JavaScript
+
+let num;
+num = 0;
+// or let num = 0;
+
+// allows us to define multiple types of values also
+
+// Const : special type if variable once defined cannot be mutated (Changed)
+
+const PI = 3.14;
+// PI = 3.15 ❌❌❌ error
