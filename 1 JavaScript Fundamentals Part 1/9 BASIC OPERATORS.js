4a1653899791c7f8b68d4e6188e388572c132f1b
Divyansh Jain <divyansh200323@gmail.com>
2024-02-28 22:09:10 +0530

    S1L9 done

---
+++ 1 JavaScript Fundamentals Part 1/9 BASIC OPERATORS.js
@@ -0,0 +1,39 @@
+/*
+const ageJonas = 2037 - 1991;
+const ageSarah = 2037 - 2018;
+
+console.log(ageJonas, ageSarah);
+*/
+
+// always use const until if we want to modify some data
+
+// here 2037 is repeated and we ca use that also as a variable, so
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas*2, ageJonas/10 , 2**3);
+
+// concatination of Strings
+
+const firstName = 'Divyansh';
+const lastName = 'Jain';
+console.log(firstName+' '+lastName);
+
+let x = 10+5;
+x= ++x;
+x *= 20;
+
+
+console.log(x);
+
+console.log(ageJonas < ageSarah);
+console.log(ageJonas > 18);
+console.log(ageSarah > 19);
+console.log(ageSarah >= 19);
+
+// predict answer
+// 15 , 33 ,0
