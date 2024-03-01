35e0d141dce6f813765014606e487ced5a47294b
Divyansh Jain <divyansh200323@gmail.com>
2024-03-01 19:17:36 +0530

    S1L13 done

---
+++ 1 JavaScript Fundamentals Part 1/13 TAKING DECISIONS: IF ELSE STATEMENTS.js
@@ -0,0 +1,20 @@
+const birthYear = 2011;
+const now = 2024;
+if((now - birthYear) >= 18)
+    console.log(`tu gaddi chala sakta hai`);
+else
+    console.log(`abe beta thereko abhi ${18 - (now - birthYear)} saal tak chabi bhi nhi pakadna, Smaja!`);
+
+
+// condition ke hisaab se jantae ha apun
+
+
+const a = true;
+const b = false;
+
+if(a !=  b){
+    console.log('true')
+}
+else{
+    console.log('false');
+}
