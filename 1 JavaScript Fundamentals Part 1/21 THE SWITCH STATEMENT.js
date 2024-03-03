c68041c6e0dd447c74a2eb973eb87b1602e46012
Divyansh Jain <divyansh200323@gmail.com>
2024-03-04 00:10:39 +0530

    task done

---
+++ 1 JavaScript Fundamentals Part 1/21 THE SWITCH STATEMENT.js
@@ -0,0 +1,51 @@
+// switch janta hai ab kuch naya jaan uske baare mai
+// wo sirf aur sirf strictly equals follow krta hai
+
+
+const day = 'Thursday';
+
+switch(day){
+    case 'Monday':
+                    console.log('Monday');
+                    break;
+    case 'Tuesday':
+                    console.log('Tuesday');
+                    break;
+    case 'Wednesday':
+                    console.log('Wednesday');
+                    break;
+    case 'Thursday':
+                    console.log('Thursday');
+                    break;
+    case 'Friday':
+                    console.log('Friday');
+                    break;
+    case 'Saturday':
+                    console.log('Saturday');
+                    break;
+    default:
+                    console.log('Chutti');
+    
+}
+
+if(day === 'Monday'){
+    console.log(day);
+}
+else if(day === 'Tuesday'){
+    console.log(day);
+}
+else if(day === 'Wednesday'){
+    console.log(day);
+}
+else if(day === 'Thursday'){
+    console.log(day);
+}
+else if(day === 'Friday'){
+    console.log(day);
+}
+else if(day === 'Saturday'){
+    console.log(day);
+}
+else {
+    console.log('Chutti');
+}
