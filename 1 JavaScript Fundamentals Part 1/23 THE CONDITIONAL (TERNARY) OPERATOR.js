d3662c7fe6cafa2af8a84470de7257aa1cb567d3
Divyansh Jain <divyansh200323@gmail.com>
2024-03-04 00:31:02 +0530

    done

---
+++ 1 JavaScript Fundamentals Part 1/23 THE CONDITIONAL (TERNARY) OPERATOR.js
@@ -0,0 +1,28 @@
+/* Ternary Operator
+Ye Janta Huu 
+Syntax : Condition ? value when true : for false
+
+ex :
+    
+    and we can use it with multiple type of values
+
+    ex : above19 = age > 19 ? true : false;
+
+    let stmt;
+    stmt = age > 18 ? 'Drive' : "Baccha";
+    console.log(stmt);
+*/
+
+
+let age = 2024 - 2003;
+    let above19 = age > 19 ? true : false;
+
+    let stmt;
+    stmt = age > 18 ? 'Drive' : "Baccha";
+    console.log(stmt);
+
+
+    // we can use in this way too
+
+age = 22;
+console.log(`Shaadi aur Single : ${age > 21 ? 'Shaadi' : 'Single'}`);
