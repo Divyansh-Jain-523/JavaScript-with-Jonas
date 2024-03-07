e5f0b2d1b65671fec6adb422ece1be8d6d73bf57
Divyansh Jain <divyansh200323@gmail.com>
2024-03-08 02:57:00 +0530

    gyaan

---
+++ 3 DEVELOPER SKILLS & EDITOR SETUP/03 LEARNING HOW TO CODE.js
@@ -0,0 +1,9 @@
+// bhaiya ki gyaan ke baatein achi lgi aur tum log bhi jaano
+
+// 1 coding chori se na karo sbko batao ab halla krne nhi bol ra compete kro logo ko ya doubt solve ya kuch aur karo
+
+// 2 projects banao chote ho ya bade ek din mai to developer ban nhi jaoge
+
+// 3 consistency rakho wo bhi ek course pe phele baad mai dusre dekhna
+
+// baaki kuch nhi 
