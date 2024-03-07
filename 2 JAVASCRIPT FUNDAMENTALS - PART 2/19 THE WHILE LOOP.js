bc254a6dbac28cfcf4044d0fbc3cf4163e676b6c
Divyansh Jain <divyansh200323@gmail.com>
2024-03-08 01:57:37 +0530

    mostly skipped

---
+++ 2 JAVASCRIPT FUNDAMENTALS - PART 2/19 THE WHILE LOOP.js
@@ -0,0 +1,12 @@
+// while loop to aata hai but ab file khadi dene ka man nhi ho ra to thoda comparision seekhe
+
+"use strict";
+
+console.log(typeof "" === 'string');
+console.log(2 === 2);
+console.log(typeof(2 - 1) === 'number');
+// more than enough
+console.log(typeof undefined === 'undefined')
+
+// kuch bhi datatype se comapare krna ho to hamesha use string mai hi likhna
+// aur kuch try mat krna error dega
