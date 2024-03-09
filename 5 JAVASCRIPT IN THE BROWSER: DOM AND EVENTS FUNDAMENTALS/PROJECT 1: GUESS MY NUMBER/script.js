42a475d9475ff1d65a56ea3e3f5bf1be9d47949d
Divyansh Jain <divyansh200323@gmail.com>
2024-03-09 21:42:47 +0530

    guessMyNum 👍

--- 5 JAVASCRIPT IN THE BROWSER: DOM AND EVENTS FUNDAMENTALS/PROJECT 1: GUESS MY NUMBER/script.js
+++ 5 JAVASCRIPT IN THE BROWSER: DOM AND EVENTS FUNDAMENTALS/PROJECT 1: GUESS MY NUMBER/script.js
@@ -1,7 +1,7 @@
-const msgDisplay = document.querySelector(".message");
-const scoreDisplay = document.querySelector(".score");
+const msgDisplay = (message) => {document.querySelector(".message").textContent = message};
+const scoreDisplay = (message) => {document.querySelector(".score").textContent = message};
+const highScoreDisplay = (message) => {document.querySelector(".highscore").textContent = message};
 const guessValue = document.querySelector(".guess");
-const highScoreDisplay = document.querySelector(".highscore");
 const btnCheck = document.querySelector(".check");
 let randomNum = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
@@ -9,13 +9,14 @@ let highScore = 0;
 let userInp;
 
 const highScoreUpdater = () => {
-  highScoreDisplay.textContent = Math.max(highScore, score);
+  highScore = Math.max(highScore, score);
+  highScoreDisplay(highScore);
 };
 const scoreDecrement = () => {
-  scoreDisplay.textContent = --score;
+  scoreDisplay(--score);
 };
 const winnerFunction = () => {
-    msgDisplay.textContent = "Yay! 🎉 You are Right 🎉";
+    msgDisplay("Yay! 🎉 You are Right 🎉");
     document.querySelector("body").style.backgroundColor = "#60b347";
     btnCheck.textContent = "Again!";
     btnCheck.addEventListener('click',checkBtn);
@@ -25,7 +26,7 @@ const winnerFunction = () => {
 const lostFunction = () =>{
   document.querySelector('body').style.backgroundColor = '#CC0000';
   btnCheck.textContent = 'Again!';
-  msgDisplay.textContent = 'You Lost! 😞'
+  msgDisplay('You Lost! 😞')
 }
 const checkBtn = () => {
   if (btnCheck.textContent === "Again!") {
@@ -37,28 +38,26 @@ const checkBtn = () => {
   }
   else {
     userInp = Number(guessValue.value);
-    if (userInp && score > 0) {
+    if (userInp > 0 && userInp <= 20) {
       if (userInp === randomNum) {
         winnerFunction();
-      } else if (userInp > randomNum && userInp <= 20) {
-        msgDisplay.textContent = "Guess a smaller Number";
+      } else if (userInp !== randomNum && userInp <= 20) {
+        msgDisplay("Guess a smaller Number");
         scoreDecrement();
       } else if (userInp < randomNum && userInp > 0) {
-        msgDisplay.textContent = "Guess a bigger Number";
+        msgDisplay("Guess a bigger Number");
         scoreDecrement();
-      } else msgDisplay.textContent = "Error! Range lies from 1 to 20";
+      } 
     }
-    else {
-      msgDisplay.textContent = "Enter a Number greater than 0";
-    }
+    else msgDisplay("Error! Range lies from 1 to 20");
   }
 };
 const againBtn = () => {
   score = 20;
-  msgDisplay.textContent = "Start guessing...";
+  msgDisplay("Start guessing...");
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
-  scoreDisplay.textContent = score;
+  scoreDisplay(score);
   document.querySelector(".number").textContent = '?';
   btnCheck.textContent = "Check!";
   randomNum = Math.trunc(Math.random() * 20 + 1);
