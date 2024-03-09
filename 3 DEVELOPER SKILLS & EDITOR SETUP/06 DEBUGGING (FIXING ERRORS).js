a872da7e65d7082aeca8bf536d7b95b6678966d0
Divyansh Jain <divyansh200323@gmail.com>
2024-03-09 06:01:22 +0530

    revisit

---
+++ 3 DEVELOPER SKILLS & EDITOR SETUP/06 DEBUGGING (FIXING ERRORS).js
@@ -0,0 +1,4 @@
+// Software bug: Defect or problem in a computer program.
+// Basically, any unexpected or unintended behavior of a computer program is a software bug.
+
+// so it is better to remove them
---
+++ 3 DEVELOPER SKILLS & EDITOR SETUP/07 DEBUGGING WITH THE CONSOLE AND BREAKPOINTS.js
@@ -0,0 +1,10 @@
+// using breakpoint is a great apporch espacially for loops where we can monitor each and every itrearion
+
+// ex:
+for(let i = 0 ; i < 9  ; i++){
+    debugger; // this will works everywhere may be
+    console.log(i);
+    // this red dot will either work if we code on browser itself or we run this code as we did in JAVA
+}
+
+// bhai ye concept ni aaya 😢
