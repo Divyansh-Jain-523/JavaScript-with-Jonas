1d1d434f75cd8f46dfda4d497fd563ff69eac45b
Divyansh Jain <divyansh200323@gmail.com>
2024-04-02 00:39:43 +0530

    .

---
+++ 11 WORKING WITH ARRAYS/05 MAP.js
@@ -0,0 +1,7 @@
+// returns an array with new elements that you described as 
+
+const arr = [1,2,3,4,5,6];
+const arrNew = arr.map(ele => ele**2);
+// this will return sqaure of entire array
+
+console.log(arr,arrNew);
